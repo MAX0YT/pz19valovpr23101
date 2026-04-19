3cf5910e5befb1e4b96bd4a596f28ad353a9aedc
Max0yt <valov.maks.2008@gmail.com>
2026-04-19 21:23:42 +0700

    final3

--- app/src/main/java/com/example/pz19valovpr23101/MainActivity.java
+++ app/src/main/java/com/example/pz19valovpr23101/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pz19valovpr23101;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import android.app.DatePickerDialog;
@@ -12,10 +14,12 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.DialogFragment;
 
 import java.lang.ref.Cleaner;
 import java.util.Calendar;
@@ -54,6 +58,14 @@ public class MainActivity extends AppCompatActivity {
                     dateAndTime.get(Calendar.HOUR_OF_DAY),
                     dateAndTime.get(Calendar.MINUTE), true)
                     .show();
+        } else if (id == R.id.btn_dialog){
+            try {
+                CustomDialogFragment dialog = new CustomDialogFragment();
+                dialog.show(getSupportFragmentManager(), "custom");
+            } catch (Exception e) {
+                e.printStackTrace();
+                // или показать Toast
+            }
         }
     }
     TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
@@ -73,4 +85,18 @@ public class MainActivity extends AppCompatActivity {
         setInitialDateTime();
       }
     };
+    public static class CustomDialogFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+            return builder
+                    .setTitle("Диалоговое окно")
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setMessage("Для закрытия окна нажмите ОК")
+                    .setPositiveButton("OK", null)
+                    .setNegativeButton("Отмена", null)
+                    .create();
+        }
+    }
 }
